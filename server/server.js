e2519a643b3b79401611401ccc8c35ee53452728
lpaspb <lpaspb97@gmail.com>
2017-12-15 00:44:02 +0300

    server fix-2

--- server/server.js
+++ server/server.js
@@ -3,17 +3,17 @@ const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-console.log(__dirname + '/../public');
-console.log(publicPath);
+
 
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
 
 
 })
+*/
 
 app.listen(port, () => {
   console.log('Server is up on ' + port);
